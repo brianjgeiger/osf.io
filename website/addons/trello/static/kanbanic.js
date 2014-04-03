302adaaa5637bd725407d2c82a097a976f044b3c
Brian J. Geiger <bgeiger@pobox.com>
2014-04-03 15:50:35 -0400

    Replaced regex search and replace for website links in comments and descriptions and instead am using Autolinker.js to do the same. It seems a much better and more robust solution.

--- website/addons/trello/static/kanbanic.js
+++ website/addons/trello/static/kanbanic.js
@@ -245,8 +245,8 @@ function addAttachmentInfo(data){
 
 
 function replaceURLWithHTMLLinks(text) {
-  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-  return text.replace(exp,"<a href='$1'>$1</a>");
+    var linkedText = Autolinker.link( text, {truncate: 50, newWindow: true } );
+  return linkedText;
 }
 
 //
