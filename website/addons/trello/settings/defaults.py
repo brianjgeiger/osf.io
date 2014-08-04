ae7c5e1169ed0d68ce9f49bb37ce41479e05eac9
Brian J. Geiger <bgeiger@pobox.com>
2014-08-04 15:49:09 -0400

    Added in some defaults that were missing.

--- website/addons/trello/settings/defaults.py
+++ website/addons/trello/settings/defaults.py
@@ -1,6 +1,7 @@
 # trello application credentials
 CLIENT_ID = None
 CLIENT_SECRET = None
-
+APP_NAME = 'Open Science Framework'
+APP_NAME_URL = 'Open+Science+Framework'
 # Trello access scope
 SCOPE = ['read','write']
